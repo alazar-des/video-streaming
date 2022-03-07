84e13d70834ee588dbf7a641f2047878a8b739ce
alazar-des <alazar.des@gmail.com>
2022-03-07 20:28:32 +0300

    App componets

---
+++ client/src/components/App/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+    Route,
+    BrowserRouter as Router,
+    Routes,
+} from "react-router-dom";
+import Home from '../Home/Home';
+import Player from '../Player/Player';
+import VideoUpload from '../VideoUpload/VideoUpload';
+import Navbar from '../Navbar/Navbar';
+import Login from '../Login/Login';
+import Signup from '../Signup/Signup';
+import useToken from './useToken';
+import './App.css';
+
+function App() {
+    const { token, setToken } = useToken();
+
+    return (
+            <Router>
+              <Navbar />
+                <Routes>
+                  <Route exact path="/" element={<Home />}></Route>
+                  <Route path="/video/:id" element={<Player />}></Route>
+                  <Route path="/upload" element={<VideoUpload token={token} setToken={setToken} />}></Route>
+                  <Route path="/login" element={<Login setToken={setToken}/>}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+              </Routes>
+            </Router>
+    );
+}
+
+export default App;
